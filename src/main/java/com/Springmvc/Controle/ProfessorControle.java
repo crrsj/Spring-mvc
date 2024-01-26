4eedb6e12984d861c91de0556bc2a7a89ed2f63c
crrsj <crrsj1@gmail.com>
2024-01-26 11:19:06 -0300

    ajustes no metodo salvar,implementando excluir e validações

--- src/main/java/com/Springmvc/Controle/ProfessorControle.java
+++ src/main/java/com/Springmvc/Controle/ProfessorControle.java
@@ -1,9 +1,13 @@
 package com.Springmvc.Controle;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +20,8 @@ import com.Springmvc.Model.Professor;
 import com.Springmvc.Model.Telefone;
 import com.Springmvc.Repositorio.ProfessorRepositorio;
 import com.Springmvc.Repositorio.TelefoneRepositorio;
+
+import jakarta.validation.Valid;
 
 
 
@@ -37,9 +43,24 @@ public class ProfessorControle {
 		return mv;
 		
 	}
-	@RequestMapping(method = RequestMethod.POST,value = "**/deletarprofessor")
-	public ModelAndView salvar(Professor professor) {
+	@RequestMapping(method = RequestMethod.POST,value = "**/salvarprofessor")
+	public ModelAndView salvar(@Valid Professor professor,BindingResult result) {
+		
+		if(result.hasErrors()) {
+			var mv = new ModelAndView("cadastro/cadastroprofessor");
+			Iterable<Professor>prof  = repositorio.findAll();
+			mv.addObject("professores",prof);
+			mv.addObject("professorobj",professor);
+			List<String> msg = new ArrayList<String>();
+			for(ObjectError obj : result.getAllErrors() ) {
+				msg.add(obj.getDefaultMessage());  //vem das anotações do @NotEmpty
+			}
+			mv.addObject("msg",msg);
+			return mv;
+		}
+		
 		repositorio.save(professor);
+		
 		var mv = new ModelAndView("cadastro/cadastroprofessor");
 		Iterable<Professor>prof  = repositorio.findAll();
 		mv.addObject("professores",prof);
@@ -99,4 +120,16 @@ public class ProfessorControle {
 		mv.addObject("telefones",telRepositotio.getTelefones(idprofessor));
 		return mv;
 	}
+	
+	@GetMapping("/removertelefone/{idtelefone}")
+	public ModelAndView excluirTelefone(@PathVariable("idtelefone") Long idtelefone) {	
+		var professor = telRepositotio.findById(idtelefone).get().getProfessor();
+		telRepositotio.deleteById(idtelefone);
+		var mv = new ModelAndView("cadastro/telefones");
+		mv.addObject("professorobj",professor);
+		mv.addObject("telefones",telRepositotio.getTelefones(professor.getId()));
+		return mv;
+		
+	}
+	
 }
--- src/main/java/com/Springmvc/Model/Professor.java
+++ src/main/java/com/Springmvc/Model/Professor.java
@@ -3,12 +3,15 @@ package com.Springmvc.Model;
 import java.io.Serializable;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,9 +29,15 @@ public class Professor implements Serializable {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+    @NotNull(message = "Nome não pode ser nulo")
+    @NotEmpty(message = "Nome não pode ser vazio")
 	private String nome;
+    @NotNull(message = "Disciplina não pose ser nula")
+    @NotEmpty(message = "Disciplina não pode ser vazia")
 	private String disciplina;
+    @NotNull(message ="Nível não pode ser nulo")
+    @NotEmpty(message = "Nível não pode ser vazio")
 	private String nivel;
-	@OneToMany(mappedBy = "professor")
+	@OneToMany(mappedBy = "professor",orphanRemoval = true, cascade = CascadeType.ALL)
 	private List<Telefone>telefones;
 }
